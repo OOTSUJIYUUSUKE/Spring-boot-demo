e2fe4e43e85369821bc1c4a7959c868ad3e0b739
OOTSUJIYUUSUKE <tsujichan221@gmail.com>
2023-04-01 20:55:18 +0900

    HelloWorldに言語切り替え機能と時刻表示機能を追加

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,37 @@
 package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+
+    public String hello(@RequestParam(value = "country", defaultValue = "jp") String value) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String strDateTime = localDateTime.format(formatter);
+        if (value.equals("us")) {
+            return String.format("Hello World! It's %s.", strDateTime);
+        }
+        else if (value.equals("fr")) {
+            return String.format("Bonjour le monde! Il est %s.", strDateTime);
+        }
+        else if (value.equals("kr")) {
+            return String.format("안녕하세요 세계! 지금 %s 시입니다.", strDateTime);
+        }
+        else {
+            return String.format("こんにちは! 現在時刻は%sです。", strDateTime);
+        }
+    }
 }
